2a1de39ac75c46768baaa0090c4b3b77c4519271
ramasubbaiah <ramasubbaiah@LAPTOP-8FTKGC45>
2022-11-11 11:16:20 +0530

    first maven to git

---
+++ src/main/java/ACOEDEVOPS/MH1A0570MJ2/App.java
@@ -0,0 +1,13 @@
+package ACOEDEVOPS.MH1A0570MJ2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
